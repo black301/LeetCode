daa9d131c9e8bd1cbae7471a638b17979283380e
Ali Essam <112704461+black301@users.noreply.github.com>
2025-01-09 04:06:26 +0200

    [2185] [Time Beats: 58.95%] [Memory Beats: 10.53%] - LeetPush

---
+++ DCP-01-25/2185-Counting-Words-With-a-Given-Prefix.cs
@@ -0,0 +1,12 @@
+ public class Solution
+ {
+     public int PrefixCount(string[] words, string pref)
+     {
+         int count = 0;
+         foreach (var word in words) { 
+             if(word.StartsWith(pref))
+                 count++;
+         }
+         return count;
+     }
+ }
