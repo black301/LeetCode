f7dcd55fdb15a2859cb80d3620043d10d3abd0a4
Ali Essam <112704461+black301@users.noreply.github.com>
2025-01-13 02:13:15 +0200

    [1974] [Time Beats: 100.00%] [Memory Beats: 7.41%] - LeetPush

--- 1974-Minimum-Time-to-Type-Word-Using-Special-Typewriter.cs
+++ 1974-Minimum-Time-to-Type-Word-Using-Special-Typewriter.cs
@@ -1,17 +1,13 @@
  public class Solution
- {
-     public int MinTimeToType(string word)
-     {
-         char current = 'a';
-         int result = 0;
-         foreach (char c in word) { 
-             if (c != current) 
-             result += Math.Min(Math.Abs(current-c),Math.Min(Math.Abs((current-'a')+'z'-c)+1, Math.Abs((current - 'z') + 'a' - c) + 1));
-             //Console.WriteLine($"Math.Abs({current}-{c}) {Math.Abs(current - c)} Math.Abs(({current}-'a')+'z'-{c})+1): {Math.Abs((current - 'a') + 'z' - c)}");
-             current = c;
-             result++;       
-             //Console.WriteLine(result);
-         }
-         return result;
-     }
+ {public int MinTimeToType(string word)
+{
+    char current = 'a';
+    int result = 0;
+    foreach (char c in word) {
+        int second = Math.Abs(current - c);
+        result += Math.Min(second,26-second)+1;
+        current = c;    
+    }
+    return result;
+}
  }
