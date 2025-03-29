909b5b179d1881295c98a06e96360af4eeaa91bf
Ali Essam <112704461+black301@users.noreply.github.com>
2025-03-29 15:05:26 +0200

    [136] [Time Beats: 21.97%] [Memory Beats: 5.10%] - LeetPush

--- Single-Number.cs
+++ Single-Number.cs
@@ -1,6 +1,7 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
-        var result = nums.GroupBy(x => x).Where(x => x.Count() == 1).Select(x=>x.Key).ToList();
-return result[0];
+       
+                var result = nums.GroupBy(x => x).Where(x=>x.Count()==1);
+                return result.First().Key;
     }
 }
