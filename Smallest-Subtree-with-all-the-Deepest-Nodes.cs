674523f74a33c7d07b7f27de8d6dd4160bdedf5b
Ali Essam <112704461+black301@users.noreply.github.com>
2025-04-05 00:29:01 +0200

    [865] [Time Beats: 100.00%] [Memory Beats: 16.67%] - LeetPush

---
+++ Smallest-Subtree-with-all-the-Deepest-Nodes.cs
@@ -0,0 +1,29 @@
+ public class Solution
+ {
+     public TreeNode SubtreeWithAllDeepest(TreeNode root)
+     {
+         return I_HATE_TREES(root).Item1;
+     }
+     public Tuple<TreeNode, int> I_HATE_TREES(TreeNode ROOT)
+     {
+         if (ROOT == null)
+         {
+             return new Tuple<TreeNode, int>(null, 0);
+         }
+         var left = I_HATE_TREES(ROOT.left);
+         var right = I_HATE_TREES(ROOT.right);
+         if (left.Item2 > right.Item2)
+         {
+             return new Tuple<TreeNode, int>(left.Item1, left.Item2 + 1);
+         }
+         else if (left.Item2 < right.Item2)
+         {
+             return new Tuple<TreeNode, int>(right.Item1, right.Item2 + 1);
+         }
+         else
+         {
+             return new Tuple<TreeNode, int>(ROOT, left.Item2 + 1);
+         }
+     }
+
+ }
