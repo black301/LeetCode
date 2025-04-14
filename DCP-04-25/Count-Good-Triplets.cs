a693a46d4ef77e5b83c8b8d5d2a727c0b797a621
Ali Essam <112704461+black301@users.noreply.github.com>
2025-04-14 22:47:08 +0200

    [1534] [Time Beats: 34.48%] [Memory Beats: 50.86%] - LeetPush

---
+++ DCP-04-25/Count-Good-Triplets.cs
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public int CountGoodTriplets(int[] arr, int a, int b, int c)
+    {
+        int n = arr.Length;
+        int counter = 0;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                for(int k = j + 1; k < n; k++)
+                {
+                    if (Math.Abs(arr[i] - arr[j]) <= a
+                        && Math.Abs(arr[j] - arr[k]) <= b
+                        &&Math.Abs( arr[i] - arr[k]) <= c)
+                    {
+                        counter++;
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+}
