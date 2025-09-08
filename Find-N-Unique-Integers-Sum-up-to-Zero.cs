450e25a238098cf3ccddebe9eff3d7ec10b61d28
Ali Essam <3liblackcat1@gmail.com>
2025-09-08 12:16:00 +0300

    [1304] [Time Beats: 100.00%] [Memory Beats: 22.81%] - LeetPush

---
+++ Find-N-Unique-Integers-Sum-up-to-Zero.cs
@@ -0,0 +1,15 @@
+  public class Solution
+  {
+      public int[] SumZero(int n)
+      {
+          List<int> result = new List<int>();
+          for (int i = 1; i <= n / 2; i++)
+              {
+                  result.Add(i);
+                  result.Add(-i);
+              }
+          if (n % 2 != 0)
+              result.Add(0);
+          return result.ToArray();
+      }
+  }
